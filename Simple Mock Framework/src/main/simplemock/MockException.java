f0daaf42aa8606892428a45e789a230160e16d2b
kymaka <seafth345@gmail.com>
2022-03-28 22:52:39 +0700

    Added missing exception

---
+++ Simple Mock Framework/src/main/simplemock/MockException.java
@@ -0,0 +1,21 @@
+package main.simplemock;
+
+/**
+ * Used within the package to wrap around checked exceptions and convert them
+ * into runtime exceptions
+ */
+public class MockException extends RuntimeException {
+
+  private static final long serialVersionUID = 4645180969088358242L;
+
+  private MockException(Throwable cause) {
+    super(cause);
+  }
+
+  public static MockException wrap(Throwable cause) {
+    if (cause instanceof MockException) {
+      return (MockException) cause;
+    }
+    return new MockException(cause);
+  }
+}
